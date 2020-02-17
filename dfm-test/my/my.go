afcd16e11e51ff93decf3598c4d3143b8933cff7
Min65535 <22154915+Min65535@users.noreply.github.com>
2020-02-17 21:26:51 +0800

    add codes

--- dfm-test/my/my.go
+++ dfm-test/my/my.go
@@ -7,6 +7,9 @@ import (
 	"runtime"
 	"strings"
 	"errors"
+	"fmt"
+	"time"
+	"log"
 )
 
 //获取可执行程序当前路径
@@ -17,7 +20,7 @@ func pathGet() (path string, err error) {
 	}
 
 	path, err = filepath.Abs(file)
-	if err!=nil{
+	if err != nil {
 		return
 	}
 	if runtime.GOOS == "windows" {
@@ -33,6 +36,56 @@ func pathGet() (path string, err error) {
 	return
 }
 
-func main()  {
+func main() {
 
+	//go Love() // 起一个协程去执行定时任务
+	go Love() // 起一个协程去执行定时任务
+
+	stop := 0
+	for {
+		fmt.Scan(&stop)
+		if stop == 1 {
+			break
+		}
+	}
+}
+
+func ping() {
+	var b string
+	goos := runtime.GOOS
+	switch goos {
+	case "windows":
+		cmd := exec.Command("ping", "172.16.0.30", "-n", "2")
+		stdout, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Println("StdoutPipe err:", err.Error())
+			return
+		}
+		b = string(stdout)
+	case "linux":
+		cmd := exec.Command("ping", "172.16.0.30", "-c", "2")
+		stdout, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Println("StdoutPipe err:", err.Error())
+			return
+		}
+		b = string(stdout)
+	}
+	log.Println("the result of ping:", b)
+}
+
+func Love() {
+	timer := time.NewTimer(8 * time.Minute) // 新建一个Timer
+	//timer := time.NewTimer(2 * time.Second) // 新建一个Timer
+
+	for {
+		select {
+		case <-timer.C:
+			fmt.Println("my timer start")
+			ping()
+			timer.Reset(8 * time.Minute) // 上一个when执行完毕重新设置
+			//timer.Reset(2 * time.Second) // 上一个when执行完毕重新设置
+		}
+	}
+	return
 }
