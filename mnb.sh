a65c965b266cecb39e23d86f4d09799112b9abd0
min <alan.du@ppiaas.cn>
2021-02-01 21:00:35 +0800

    cancle logs

--- mnb.sh
+++ mnb.sh
@@ -18,13 +18,3 @@
 ###do
 ###  rm -rf ${i}
 ###done
-
-
-2021-02-01 19:50:57.212	info	flowpool-86dd7f56c5-46wgh##newgitlab.com/blockchain/flowpool/internal/flowpool/service.(*Service).LoginApp:67	LoginApp#checkUser	{"phone": "13879781228", "error": "用户还没授权"}
-
-
-2021-02-01 19:50:57.212	error	flowpool-86dd7f56c5-46wgh##newgitlab.com/blockchain/go-ms-toolkit/happyerr.Handle:51	FlowpoolSvc	{"sourceID": 0, "trace": null, "error": "用户还没授权"}
-2021-02-01 19:50:57.212	info	flowpool-86dd7f56c5-46wgh##newgitlab.com/blockchain/flowpool/internal/flowpool/service.(*Service).LoginApp:67	LoginApp#checkUser	{"phone": "13879781228", "error": "用户还没授权"}
-
-
-2020-04-01 16:52:43.918 warn    gapigateway-deployment-548485fb6b-fkb8b    main.Ha:37     RegisterErr  {"error": "fail to register at 2020-04-01 16:52:43"}
