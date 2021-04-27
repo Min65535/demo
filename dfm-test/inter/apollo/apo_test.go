d7bd91cdc314742c5ec7a248fb1f8afed7b5fb39
min <alan.du@ppiaas.cn>
2021-04-27 11:40:32 +0800

    add

--- dfm-test/inter/apollo/apo_test.go
+++ dfm-test/inter/apollo/apo_test.go
@@ -1,8 +1,30 @@
 package apollo
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
 func TestMyApolloDemo(t *testing.T) {
+	x := [3]int{1, 2, 3}
+	go func(arr [3]int) {
+		arr[0] = 7
+		fmt.Println("x arr:", arr) // prints [7 2 3]
+	}(x)
+
+	time.Sleep(1 * time.Second)
+	fmt.Println(x) // prints [7 2 3]
+
+	y := []int{4, 5, 6}
+	go func(arr []int) {
+		arr[0] = 8
+		fmt.Println("y arr:", arr) // prints [7 2 3]
+	}(y)
+
+	time.Sleep(1 * time.Second)
+	fmt.Println(y) // prints [7 2 3]
+
 	t.Skip()
 	MyApolloDemo()
 }
