4fea5ace2c650d147c8dd52b7c295f668748f8af
min <alan.du@ppiaas.cn>
2021-05-06 16:28:38 +0800

    add

--- dfm-test/inter/apollo/apo_test.go
+++ dfm-test/inter/apollo/apo_test.go
@@ -28,3 +28,25 @@ func TestMyApolloDemo(t *testing.T) {
 	t.Skip()
 	MyApolloDemo()
 }
+
+func TestMyApolloDemo2(t *testing.T) {
+	var d uint8 = 2
+	fd:=^d
+	fmt.Printf("%016b\n",fd)
+	fmt.Printf("%016b\n", d)
+	dXOfd := d ^ (^d)
+	fmt.Printf("%016b\n", dXOfd)
+	fmt.Printf("%016b\n", dXOfd&fd)
+
+	fmt.Printf("%016b\n", d&(^d))
+	var a uint8 = 0x82
+	var b uint8 = 0x02
+	fmt.Printf("%016b [A]\n",a)
+	fmt.Printf("%016b [B]\n",b)
+	fmt.Printf("%08b (NOT B)\n",^b)
+	fmt.Printf("%08b ^ %08b = %08b [B XOR 0xff]\n",b,0xff,b ^ 0xff)
+	fmt.Printf("%08b ^ %08b = %08b [A XOR B]\n",a,b,a ^ b)
+	fmt.Printf("%08b & %08b = %08b [A AND B]\n",a,b,a & b)
+	fmt.Printf("%08b &^%08b = %08b [A 'AND NOT' B]\n",a,b,a &^ b)
+	fmt.Printf("%08b&(^%08b)= %08b [A AND (NOT B)]\n",a,b,a & (^b))
+}
