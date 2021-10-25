e1930c4d68a54bac614e3053955382c16b878b09
min <alan.du@ppiaas.cn>
2021-10-25 15:13:16 +0800

    add new script codes

--- mnb.sh
+++ mnb.sh
@@ -2,7 +2,7 @@
 
 ### 查找,例如此脚本叫mn.sh
 #root=`pwd`
-#names=`ls -lh|grep -v 'mn.sh'|grep -v 'ert.sh'|wk '{print $9}'`
+#names=`ls -lh|grep -v 'mn.sh'|grep -v 'ert.sh'|awk 'NR>1{print $9}'`
 #echo ${names}
 #for i in ${names}
 #do
@@ -14,7 +14,7 @@
 
 
 ### 删除,例如此脚本叫ert.sh
-#names=`ls -lh|grep 3月|grep -v 'ert.sh'|awk '{print $9}'`
+#names=`ls -lh|grep 3月|grep -v 'ert.sh'|awk 'NR>1{print $9}'`
 #echo ${names}
 #for i in ${names}
 #do
