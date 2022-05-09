cb14d3f4dd5ee500c3da0fde4abd2fe3d60ba88a
alan <alan@micous.com>
2022-05-09 15:51:26 +0800

    add

--- dfm-test/inter/algorithm/random_test.go
+++ dfm-test/inter/algorithm/random_test.go
@@ -1,1 +1,13 @@
 package algorithm
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewMersenneTwister(t *testing.T) {
+	mt := NewMersenneTwister(0)
+	for i := 0; i < 10; i++ {
+		fmt.Println(mt.Rand())
+	}
+}
