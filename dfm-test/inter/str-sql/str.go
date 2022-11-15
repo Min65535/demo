858c7f228a062b43ce4dd8dab7fa0e457237d0b5
alan <alan@micous.com>
2022-11-15 16:56:14 +0800

    add

--- dfm-test/inter/str-sql/str.go
+++ dfm-test/inter/str-sql/str.go
@@ -3,6 +3,8 @@ package str_sql
 import (
 	"errors"
 	"fmt"
+	"github.com/min65535/demo/dfm-test/inter/timer/inner"
+	"time"
 )
 
 func GetSqlStr(fromId, toId int64) (string, error) {
@@ -19,4 +21,27 @@ func GetSqlStr(fromId, toId int64) (string, error) {
 	fmt.Println("by:", by)
 	str = str + by + ";"
 	return str, nil
+}
+
+func TimerMy() {
+	tr := inner.NewTimer()
+	now := time.Now()
+	nowUnix := now.Unix()
+	latestStart := 1667491200
+	nextStart := time.Unix(int64(latestStart), 0).Add(time.Second * time.Duration(2592000))
+	nextStartUnix := nextStart.Unix()
+	fmt.Println("nextStartUnix: ", nextStartUnix)
+	remainUnix := nextStartUnix - nowUnix
+	fmt.Println("remainUnix: ", remainUnix)
+	tr.ResetTimer(int32(remainUnix))
+	for {
+		select {
+		case <-tr.Timer.C:
+			fmt.Println("do do do")
+			// if err = tuc.DoUpdateUsersExpAndGameSeasonRcds(); err != nil {
+			// 	log.Errorf("SeasonRun#DoUpdateUsersExpAndGameSeasonRcds#err:%s", err.Error())
+			// }
+			tr.ResetTimer(int32(2592000))
+		}
+	}
 }
